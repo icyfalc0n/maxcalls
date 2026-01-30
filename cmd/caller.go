cdcba2d5c2a0e9ac2e4cf4b9b1d86c70b9b01ca4
icyfalc0n <icyfalc0n@proton.me>
2026-01-31 02:05:56 +0300

    feat!: added udp connection redirection

--- cmd/caller.go
+++ cmd/caller.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
+	"sync"
 
 	"github.com/icyfalc0n/maxcalls"
 	"github.com/spf13/cobra"
 )
+
+var callerListenAddr string
 
 var callerCmd = &cobra.Command{
 	Use:   "caller [calltaker_id]",
@@ -30,14 +34,75 @@ var callerCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(callerCmd)
+	callerCmd.Flags().StringVarP(&callerListenAddr, "listen", "l", ":2000", "UDP address to listen on")
 }
 
 func runCaller(calls maxcalls.Calls, calltakerID string) {
 	fmt.Printf("Caller ID: %s\n", calls.ExternalUserId())
 	fmt.Printf("Calling: %s\n", calltakerID)
 
-	_, err := calls.Call(calltakerID)
+	conn, err := calls.Call(calltakerID)
 	if err != nil {
 		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Listening on %s and redirecting to call...\n", callerListenAddr)
+
+	udpAddr, err := net.ResolveUDPAddr("udp", callerListenAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer udpConn.Close()
+
+	var remoteAddr *net.UDPAddr
+	var mu sync.Mutex
+
+	// Read from UDP and write to Call
+	go func() {
+		buf := make([]byte, 1500)
+		for {
+			n, addr, err := udpConn.ReadFromUDP(buf)
+			if err != nil {
+				fmt.Printf("ReadFromUDP error: %v\n", err)
+				return
+			}
+
+			mu.Lock()
+			remoteAddr = addr
+			mu.Unlock()
+
+			_, err = conn.Write(buf[:n])
+			if err != nil {
+				fmt.Printf("Connection Write error: %v\n", err)
+				return
+			}
+		}
+	}()
+
+	// Read from Call and write to UDP
+	buf := make([]byte, 1500)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Connection Read error: %v\n", err)
+			break
+		}
+
+		mu.Lock()
+		addr := remoteAddr
+		mu.Unlock()
+
+		if addr != nil {
+			_, err = udpConn.WriteToUDP(buf[:n], addr)
+			if err != nil {
+				fmt.Printf("WriteToUDP error: %v\n", err)
+			}
+		}
 	}
 }
--- cmd/calltaker.go
+++ cmd/calltaker.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/icyfalc0n/maxcalls"
 	"github.com/spf13/cobra"
 )
+
+var calltakerForwardAddr string
 
 var calltakerCmd = &cobra.Command{
 	Use:   "calltaker",
@@ -29,14 +32,63 @@ var calltakerCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(calltakerCmd)
+	calltakerCmd.Flags().StringVarP(&calltakerForwardAddr, "forward", "f", "127.0.0.1:3000", "UDP address to forward packets to")
 }
 
 func runCalltaker(calls maxcalls.Calls) {
 	fmt.Printf("Call taker ID: %s\n", calls.ExternalUserId())
 	fmt.Println("Waiting for incoming calls....")
 
-	_, err := calls.WaitForCall()
+	conn, err := calls.WaitForCall()
 	if err != nil {
 		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Call received. Redirecting packets to %s...\n", calltakerForwardAddr)
+
+	targetAddr, err := net.ResolveUDPAddr("udp", calltakerForwardAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	udpConn, err := net.DialUDP("udp", nil, targetAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer udpConn.Close()
+
+	// Read from Call and write to UDP
+	go func() {
+		buf := make([]byte, 1500)
+		for {
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Printf("Connection Read error: %v\n", err)
+				return
+			}
+
+			_, err = udpConn.Write(buf[:n])
+			if err != nil {
+				fmt.Printf("Write to UDP error: %v\n", err)
+				return
+			}
+		}
+	}()
+
+	// Read from UDP and write to Call
+	buf := make([]byte, 1500)
+	for {
+		n, err := udpConn.Read(buf)
+		if err != nil {
+			fmt.Printf("Read from UDP error: %v\n", err)
+			break
+		}
+
+		_, err = conn.Write(buf[:n])
+		if err != nil {
+			fmt.Printf("Connection Write error: %v\n", err)
+			break
+		}
 	}
 }
